cbd629fed12d17e39089849a9f2be66a6ee78ab3
Shimon Brathwaite <46653837+Shimon242@users.noreply.github.com>
2025-12-29 23:31:15 -0500

    Rename resources in main.tf for clarity

--- Objective2/main.tf
+++ Objective2/main.tf
@@ -10,26 +10,26 @@ locals {
 
 # Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.resource_group_name}-vnet"
+  name                = "${var.resource_group_name}-vnet2"
   address_space       = ["10.0.0.0/16"]
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "default"
+  name                 = "default2"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "${var.resource_group_name}-nic"
+  name                = "${var.resource_group_name}-nic2"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "internal2"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
